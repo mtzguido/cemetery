c02c3176ca5cb35a7c81355436323c7e5ec99801
Guido Martínez <mtzguido@gmail.com>
2015-03-03 20:29:07 -0300

    Translate: tidy up

--- Translate.hs
+++ Translate.hs
@@ -8,16 +8,37 @@ import Control.Monad.Error
 import Data.Map.Strict as M
 import Common
 
+-- Monad definition
+
 type NameEnv = M.Map VarName A.Type
 
 data TransState = TSt { envs :: [NameEnv] }
   deriving Show
 
--- lists' map, since the Data.Map one
--- might hide it
+initState = TSt { envs = [] }
+
+type TranslateMonad = ErrorT CmtError (
+                       StateT TransState (
+                        Identity
+                      ))
+
+type TM = TranslateMonad -- Only for brevity
+
+runTranslate :: TM a -> (TransState, Either CmtError a)
+runTranslate m = let a = runErrorT m
+                     b = runStateT a initState
+                     (c, s) = runIdentity b
+                  in case c of
+                       Left e -> (s, Left e)
+                       Right a' -> (s, Right a')
+
+semanticT :: A.Prog -> (TransState, Either CmtError C.Prog)
+semanticT = runTranslate.translate
+
+-- Good ol' list map, since the Data.Map one might hide it
 lmap = Prelude.map
 
-initState = TSt { envs = [] }
+-- Environment handling functions
 
 getEnvs :: TM [NameEnv]
 getEnvs = do s <- get
@@ -39,21 +60,18 @@ addToEnv :: VarName -> A.Type -> TM ()
 addToEnv n t = do e:es <- getEnvs
                   setEnvs $ (insert n t e) : es
 
-type TranslateMonad = ErrorT CmtError (
-                       StateT TransState (
-                        Identity
-                      ))
-
-type TM = TranslateMonad -- Only for brevity
-
 add_builtins :: TM ()
 add_builtins = do return ()
+
+-- Main translation procedure
 
 translate :: A.Prog -> TM C.Prog
 translate decls = do pushEnv -- global environment
                      add_builtins -- add builtins to the environment
                      dd <- mapM tr1 decls
                      return $ concat dd
+
+-- Type inference and mapping
 
 infer :: A.Expr -> TM A.Type
 infer (A.ConstStr _)    = do return A.String
@@ -69,6 +87,8 @@ tmap A.String   = do return C.String
 tmap A.Void     = do return C.Void
 tmap A.Double   = do return C.Double
 tmap A.Bytes    = do return (C.Ptr C.Void)
+
+-- Unit translations
 
 tr1 :: A.Decl -> TM [C.Unit]
 tr1 (A.VarDecl mods n Nothing Nothing) =
@@ -112,8 +132,8 @@ tr1 (A.FunDecl { A.name = n, A.ret = r, A.args = a, A.body = b}) =
        return [FunDef (Funtype { C.name = n, C.args = argsc,
                                  C.ret = rc }) body]
 
-trexp :: A.Expr -> TM C.Expr
-trexp _ = do return $ C.Var "crap"
+-- Statement and expression translations
+-- Abandon all hope ye who enter blow this line
 
 trbody :: A.Stmt -> TM C.Block
 trbody s = do st <- trstm s
@@ -137,13 +157,5 @@ trstm (A.Return e) =
 
 trstm _ = do return C.Skip
 
-runTranslate :: TM a -> (TransState, Either CmtError a)
-runTranslate m = let a = runErrorT m
-                     b = runStateT a initState
-                     (c, s) = runIdentity b
-                  in case c of
-                       Left e -> (s, Left e)
-                       Right a' -> (s, Right a')
-
-semanticT :: A.Prog -> (TransState, Either CmtError C.Prog)
-semanticT = runTranslate.translate
+trexp :: A.Expr -> TM C.Expr
+trexp _ = do return $ C.Var "crap"
