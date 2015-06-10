ca7b070462d23c6ad3c3546a5ec1a57980a07829
Guido Martínez <mtzguido@gmail.com>
2015-06-10 17:28:03 -0300

    Optimize: change to monadic structure

--- Optimize.hs
+++ Optimize.hs
@@ -10,19 +10,43 @@
 
 module Optimize where
 
+import Control.Monad
+import Control.Monad.Identity
+
 import IR
 
+type OM = Identity
+
 optimize :: IR -> IR
-optimize p = map o_unit p
+optimize p = runIdentity $ mapM o_unit p
 
-o_unit (FunDef ft body) = FunDef ft (o_body body)
-o_unit x = x
+o_unit :: Unit -> OM Unit
+o_unit (FunDef ft b) =
+    do b' <- o_body b
+       return $ FunDef ft b'
+o_unit x =
+    do return x
 
-o_body (d, s) = (d, o_stmt s)
+o_body :: Block -> OM Block
+o_body (d, s) =
+    do s' <- o_stmt s
+       return $ (d, s')
 
-o_stmt (Assign l e) = Assign l (o_expr e)
-o_stmt (Return e) = Return (o_expr e)
-o_stmt x = x
+o_stmt :: Stmt -> OM Stmt
+o_stmt (Assign l e) =
+    do e' <- o_expr e
+       return $ Assign l e'
 
-o_expr (BinOp Plus (ConstInt l) (ConstInt r)) = ConstInt (l + r)
-o_expr e = e
+o_stmt (Return e) =
+    do e' <- o_expr e
+       return $ Return e'
+
+o_stmt x =
+    do return x
+
+o_expr :: Expr -> OM Expr
+o_expr (BinOp Plus (ConstInt l) (ConstInt r)) =
+    do return $ ConstInt (l + r)
+
+o_expr e =
+    do return e
