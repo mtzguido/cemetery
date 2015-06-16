aa4368618d6c63076c398893fc8cc741c15fd6c4
Guido Martínez <mtzguido@gmail.com>
2015-06-15 23:12:56 -0300

    Common: remove unused crap

--- Common.hs
+++ Common.hs
@@ -14,17 +14,6 @@ tr from to seq = map repl1 seq where
 traceM :: (Monad m) => String -> m ()
 traceM s = trace s (return ())
 
--- Indent every line with one tab
-indent :: String -> String
-indent s = unlines $ map ('\t':) $ lines s
-
--- Suposedly in package Maybes, but let's not introduce
--- dependencies
-fmapM_maybe :: Monad m => (a -> m b) -> Maybe a -> m (Maybe b)
-fmapM_maybe f Nothing  = do return Nothing
-fmapM_maybe f (Just a) = do v <- f a
-                            return (Just v)
-
 -- Single stub error, should be expanded later on
 data CmtError = CmtErr String
   deriving Show
