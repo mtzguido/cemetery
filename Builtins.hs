d7a17718464fbd68336e70eb968c3e951e961f5e
Guido Martínez <mtzguido@gmail.com>
2015-06-22 23:52:18 -0300

    Builtins: add "length"

--- Builtins.hs
+++ Builtins.hs
@@ -12,6 +12,7 @@ builtins = [
  ("even", e { typ = Fun [Int] Bool, ir_lv = I.LVar "__cmt_even"}),
  ("permute", e { typ = Fun [Bits, ArrT Int] Bits, ir_lv = I.LVar "__cmt_permute"}),
  ("permute_inverse", e { typ = Fun [Bits, ArrT Int] Bits, ir_lv = I.LVar "__cmt_permute_inv"}),
+ ("length", e { typ = Fun [Bits] Int, ir_lv = I.LVar "__cmt_length"}),
  ("toint", e { typ = Fun [Bits] Int, ir_lv = I.LVar "__cmt_toint"}),
  ("tobits", e { typ = Fun [Int] Bits, ir_lv = I.LVar "__cmt_tobits"})
  ]
