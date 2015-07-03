f592f9a07af97a9c02468db1ae1e75a0a53edb4a
Guido Martínez <mtzguido@gmail.com>
2015-07-02 22:51:11 -0300

    scripts: live: better formatting

--- scripts/do-live-tests.sh
+++ scripts/do-live-tests.sh
@@ -5,6 +5,7 @@ set -ue
 DIR=$1
 shift
 FLAGS="$@"
+ok=true
 
 if ! [ -d "${DIR}" ]; then
 	# Just quietly bail out
@@ -16,7 +17,7 @@ if ! [ -x ./cmt ]; then
 	exit 1
 fi
 
-printf "%-40s" "Running LIVE tests with <${FLAGS}>:"
+printf "%-40s" "Running  LIVE tests with <${FLAGS}>:"
 
 for i in ${DIR}/*; do
 	if ! [ -d "$i" ] ; then continue ; fi
@@ -24,21 +25,29 @@ for i in ${DIR}/*; do
 
 	if [ $R == . ] && ! ./cmt $FLAGS $i/code.cmt &>/dev/null; then
 		R=t
+		ok=false
 	fi
 
 	[ $R == . ] && cat $i/code.c $i/driver.c > $i/full.c
 	if [ $R == . ] && ! gcc -Wsign-compare $i/full.c -o $i/full; then
 		R=c
+		ok=false
 	fi
 
-	if [ $R == . ] && ! ./$i/full; then
+	if [ $R == . ] && ! ./$i/full &>/dev/null; then
 		R=r
+		ok=false
 	fi
 	if [ $R == . ] && ! valgrind --error-exitcode=1 --leak-check=full ./$i/full &>/dev/null; then
 		R=l
+		ok=false
 	fi
 
 	echo -n $R
 done
 
-echo " OK"
+if $ok; then
+	echo " OK"
+else
+	echo " FAILED"
+fi
