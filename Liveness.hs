e1900b11794c2742245fea0ea696dd0fd0e5a08a
Guido Martínez <mtzguido@gmail.com>
2015-07-03 18:28:26 -0300

    Liveness: copy before returning if the value isn't live
    Otherwise we'll return the same pointer and get a double free

--- Liveness.hs
+++ Liveness.hs
@@ -108,9 +108,14 @@ liv u ls lo (s:ss) =
     case s of
         Return e ->
             case e of
+                IR.LV lv | S.member lv u && not (S.member lv ls) ->
+                    let free = map Free (S.toList ls)
+                     in free ++ [Return (IR.Copy e)]
+
                 IR.LV lv ->
                     let free = map Free (S.toList $ S.delete lv ls)
                      in free ++ [s]
+
                 _ ->
                     error "liveness: return of non-lv"
 
