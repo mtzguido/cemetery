97de363950c8fa5df1feddbf7f71e41acfeb1af9
Guido Martínez <mtzguido@gmail.com>
2015-08-04 16:12:41 -0300

    mk_prologue: style fixes

--- scripts/mk_prologue.hs
+++ scripts/mk_prologue.hs
@@ -1,7 +1,6 @@
 main = do pc <- readFile "Prologue.c"
           ph <- readFile "Prologue.h"
           let contents = "module Prologue where\n\n" ++
-                          "cprologue = " ++ (show pc) ++ "\n" ++
-                          "hprologue = " ++ (show ph) ++ "\n"
+                          "cprologue = " ++ show pc ++ "\n" ++
+                          "hprologue = " ++ show ph ++ "\n"
           writeFile "Prologue.hs" contents
-          return ()
