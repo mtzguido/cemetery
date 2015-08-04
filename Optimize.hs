63dcb6d8bf6e475def9abd15d5d9e828c1d824d1
Guido Martínez <mtzguido@gmail.com>
2015-08-04 19:23:19 -0300

    Optimize: rework
    This adds structure for constant folding and bit operations clustering.
    Constant folding is done as an example for an arithmetic +

--- Optimize.hs
+++ Optimize.hs
@@ -42,15 +42,19 @@ o_unit (FunDef ft b) =
        c <- check_return_paths s
        when (not c) $ throwError $
            CmtErr ("Some paths in '" ++ name ft ++ "' do not return!")
+
+       b <- o_body b
+
        let b' = liveness b
-       b' <- o_body b'
        return $ FunDef ft b'
 
 o_unit (Decl d) = -- do nothing with declarations
     do return $ Decl d
 
-o_body b =
-    do return b
+o_body (bd, bs) =
+    do bs <- expr_opt const_fold bs
+       bs <- expr_opt clustering bs
+       return (bd, bs)
 
 ---------------------------------------------------------------------
 -- Return path checking
@@ -77,3 +81,85 @@ check_return_paths (For _ _ _ (_,b)) =
 
 check_return_paths _ =
     do return False
+
+---------------------------------------------------------------------
+-- Generic code for traversing statements until expressions
+---------------------------------------------------------------------
+
+expr_opt f (Return e) =
+    do e' <- f e
+       return $ Return e'
+
+expr_opt f (Error s) =
+    do return $ Error s
+
+expr_opt f (Seq l r) =
+    do l' <- expr_opt f l
+       r' <- expr_opt f r
+       return $ Seq l' r'
+
+expr_opt f (If c (td, ts) (ed, es)) =
+    do c' <- f c
+       ts' <- expr_opt f ts
+       es' <- expr_opt f es
+       return $ If c' (td, ts') (ed, es')
+
+expr_opt f (For i l h (bd, bs)) =
+    do l' <- f l
+       h' <- f h
+       bs' <- expr_opt f bs
+       return $ For i l' h' (bd, bs')
+
+expr_opt f Skip =
+    do return Skip
+
+expr_opt f (Assign v e) =
+    do e' <- f e
+       return $ Assign v e'
+
+expr_opt f (Free _) =
+    do error "Free before liveness analysis?"
+
+---------------------------------------------------------------------
+-- Constant folding
+---------------------------------------------------------------------
+const_fold e@(ConstInt _)     = do return e
+const_fold e@(ConstBool _)    = do return e
+const_fold e@(ConstBits _ _ ) = do return e
+const_fold e@(Copy _)         = do return e
+const_fold e@(LV _)           = do return e
+const_fold e@(Arr _)          = do error "local array on constant folding"
+
+const_fold (Slice b f t) =
+    do b' <- const_fold b
+       f' <- const_fold f
+       t' <- const_fold t
+       return $ Slice b' f' t'
+
+const_fold (Access a i) =
+    do a' <- const_fold a
+       i' <- const_fold i
+       return $ Access a' i'
+
+const_fold (Call n args) =
+    do args' <- mapM const_fold args
+       return $ Call n args'
+
+const_fold (BinOp op l r) =
+    do l' <- const_fold l
+       r' <- const_fold r
+       return $ fold_binop op l' r'
+
+const_fold (UnOp op e) =
+    do e' <- const_fold e
+       return $ fold_unop op e'
+
+fold_binop Plus (ConstInt x) (ConstInt y) = ConstInt (x+y)
+fold_binop op l r = BinOp op l r
+
+fold_unop op e = UnOp op e
+
+---------------------------------------------------------------------
+-- Clustering of bit expressions
+---------------------------------------------------------------------
+clustering e = do return e
