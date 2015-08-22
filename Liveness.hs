dc7aa6c954a61237aee15e2617a74c3a74163000
Guido Martínez <mtzguido@gmail.com>
2015-08-22 19:25:38 -0300

    Liveness: more monadize
    Small functional change, for the better

--- Liveness.hs
+++ Liveness.hs
@@ -5,18 +5,71 @@ import qualified Data.Set as S
 import Debug.Trace
 import Common
 import Control.Monad.Identity
+import Control.Monad.State
 
 ---------------------------------------------------------------------
 -- Live variable analysis
 ---------------------------------------------------------------------
 
-type LM = Identity
+data LMState = LMState {
+    live :: S.Set LValue,
+    live_out :: S.Set LValue,
+    univ :: S.Set LValue
+ }
 
-liveness (d, s) = runIdentity $ do_liv (d, s)
+type LM = StateT LMState Identity
 
+mod_live f v =
+    do s <- get
+       put (s { live = f v (live s) })
+mod_live_out f v =
+    do s <- get
+       put (s { live_out = f v (live_out s) })
+mod_univ f v =
+    do s <- get
+       put (s { univ = f v (univ s) })
+
+add_live :: LValue -> LM ()
+add_live     = mod_live     S.insert
+
+add_live_out :: LValue -> LM ()
+add_live_out = mod_live_out S.insert
+
+del_live :: LValue -> LM ()
+del_live     = mod_live     S.delete
+
+del_live_out :: LValue -> LM ()
+del_live_out = mod_live_out S.delete
+
+getS :: (LMState -> a) -> LM a
+getS f = do s <- get
+            return (f s)
+
+set_live :: S.Set LValue -> LM ()
+set_live u = do s <- get
+                put ( s { live = u })
+
+set_live_out :: S.Set LValue -> LM ()
+set_live_out u = do s <- get
+                    put ( s { live_out = u })
+
+set_univ :: S.Set LValue -> LM ()
+set_univ u = do s <- get
+                put ( s { univ = u })
+
+initState = LMState { live = S.empty, live_out = S.empty, univ = S.empty }
+
+liveness (d, s) =
+    let m = do_liv (d, s)
+        i = runStateT m initState
+        (v, _) = runIdentity i
+     in v
+
+do_liv :: ([Decl], Stmt) -> LM ([Decl], Stmt)
 do_liv (d, s) =
     do let tracked = track_set d
-       s' <- liv tracked S.empty S.empty (flatten s)
+       set_univ tracked
+       s' <- liv (flatten s)
        return (d, sfold s')
 
 track_set :: [Decl] -> S.Set LValue
@@ -131,73 +184,103 @@ unneeded ls lo ss =
         needed   = (S.union lo used) S.\\ shadowed
      in ls S.\\ needed
 
-do_frees u ls lo s =
-    let un = unneeded ls lo s
-     in (ls S.\\ un, [free un])
+do_frees s =
+    do ls <- getS live
+       lo <- getS live_out
+       let un = unneeded ls lo s
+       mapM del_live (S.toList un)
+       return [free un]
 
-liv u ls lo ss =
-    do let (ls', f) = do_frees u ls lo ss
-       ss' <- liv' u ls' lo ss
+liv ss =
+    do f <- do_frees ss
+       ss' <- liv' ss
        return (f ++ ss')
 
-liv' u ls lo [] =
+liv' [] =
     do return []
 
 -- We need to do frees manually in these two cases since we don't care
 -- about keeping the values in "lo", like we usually do
-liv' u ls lo (s@(Return (LV lv)):ss) = return $ [free (S.delete lv ls), s]
-liv' u ls lo (s@(Error _):ss) =        return [free ls, s]
-liv' u ls lo (s@(Assign l _):ss) =
-    do if shadow_s l ss
-       then liv' u ls lo ss
+liv' (s@(Return (LV lv)):ss) =
+    do ls <- getS live
+       return $ [free (S.delete lv ls), s]
+
+liv' (s@(Error _):ss) =
+    do ls <- getS live
+       return [free ls, s]
+
+liv' (s@(Assign l _):ss) =
+    do u <- getS univ
+       if shadow_s l ss
+       then liv ss
        else if S.member l u
-            then liv_assign u ls lo s ss
-            else do ss' <- liv' u ls lo ss
+            then liv_assign s ss
+            else do ss' <- liv ss
                     return (s:ss')
 
-liv' u ls lo (s@(If c t e):ss) =
-    do t' <- liv_block u ls t
-       e' <- liv_block u ls e
-       ss' <- liv u ls lo ss
+liv' (s@(If c t e):ss) =
+    do t' <- liv_block t
+       e' <- liv_block e
+       ss' <- liv ss
        return ((If c t' e'):ss')
 
-liv' u ls lo (s@(For i l h b):ss) =
-    do b' <- liv_block u ls b
-       ss' <- liv u ls lo ss
+liv' (s@(For i l h b):ss) =
+    do b' <- liv_block b
+       ss' <- liv ss
        return ((For i l h b'):ss')
 
-liv' u ls lo (s:ss) =
-    do ss' <- liv u ls lo ss
+liv' (s:ss) =
+    do ss' <- liv ss
        return (s:ss')
 
-liv_assign u ls lo s@(Assign l e) ss =
-    case s of
+liv_assign s@(Assign l e) ss =
+    do ls <- getS live
+       lo <- getS live_out
+       u <- getS univ
+       case s of
         Assign l e | S.member l ls ->
             error $ "BUG: Assigning to live value " ++ show (l, e, ls)
 
         Assign l e | shadow_s l ss ->
-            liv u ls lo ss
+            liv ss
 
         -- Avoid copies of temporaries that will be freed on
         -- the next step
         Assign l (Copy lv) | not (used_s lv ss)
                           && (not (S.member lv lo) || shadow_s lv ss)
                           && S.member lv u ->
-            liv u (S.delete lv ls) lo ((Assign l (LV lv)):ss)
+            do del_live lv
+               liv ((Assign l (LV lv)):ss)
 
         Assign l (Cluster ce as) ->
             do let un  = unneeded ls lo ss
                    un' = S.intersection un (S.fromList (map fst as))
-                   ls' = ls S.\\ un'
                    as' = map (\(lv,_) -> (lv, S.member lv un')) as
-               ss' <- liv u (S.insert l ls') lo ss
+               mapM del_live (S.toList un')
+               add_live l
+               ss' <- liv ss
                return $ [Assign l (Cluster ce as')] ++ ss'
 
         Assign l e ->
-            do ss' <- liv u (S.insert l ls) lo ss
+            do add_live l
+               ss' <- liv ss
                return $ [s] ++ ss'
 
-liv_block u ls (d, s) =
-    do let u' = S.union (S.difference u (S.fromList $ locals d)) (track_set d)
-       s' <- liv u' ls ls (flatten s)
+liv_block (d, s) =
+    do u <- getS univ
+       ls <- getS live
+       lo <- getS live_out
+
+       let u' = S.union (S.difference u (S.fromList $ locals d)) (track_set d)
+
+       set_univ u'
+       set_live ls
+       set_live_out ls
+
+       s' <- liv (flatten s)
+
+       set_univ u
+       set_live ls
+       set_live_out lo
+
        return (d, sfold s')
