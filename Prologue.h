2e5593e6a6580bb51768554824cce6347d75a1f9
Guido Martínez <mtzguido@gmail.com>
2016-09-09 13:11:57 -0300

    Prologue.h: export cmt_frombuf

--- Prologue.h
+++ Prologue.h
@@ -6,5 +6,7 @@ struct cmt_init;
 typedef struct cmt_bits *cmt_bits_t;
 
 cmt_bits_t cmt_mkbuf(const char *d, int bitlen);
+void cmt_frombuf(char *dest, cmt_bits_t b);
+
 void cmt_copy(char *dest, cmt_bits_t b);
 void cmt_free(cmt_bits_t b);
